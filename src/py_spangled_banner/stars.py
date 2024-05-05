f781c8d58cd2fc86e79dfdd503941844a44ece40
Asriel Senna <aanonyme.personne@hotmail.fr>
2024-05-05 05:21:30 +0200

    Add LayoutKind
    also formalizes the deduction of layout kind from the layout

--- src/py_spangled_banner/stars.py
+++ src/py_spangled_banner/stars.py
@@ -3,6 +3,7 @@ Computes the layout of the stars on an American flag.
 """
 
 from collections.abc import Iterable
+import enum
 from fractions import Fraction
 from functools import partial
 from numbers import Real
@@ -13,6 +14,48 @@ def _optimize_layout(x: tuple[int, int, int, int], canton_factor: Real) -> Real:
     a, b, c, d = x
     assert (c == 0) == (d == 0)
     return abs((a + c + 1) * canton_factor - (b + d + 1)) # type: ignore
+
+class LayoutKind(enum.StrEnum):
+    def __new__(cls, value, doc):
+        self = str.__new__(cls, value)
+        self._value_ = value
+        self.__doc__ = doc
+        return self
+
+    GRID = enum.auto(), """
+    The stars are arranged in a grid, like the 24-star "Old Glory" flag, or the 48-star flag.
+    """
+
+    QUINCUNX = enum.auto(), """
+    The stars are arranged in a quincunx, like the short-lived 49-star flag.
+    """
+
+    SHORT_SANDWICH = enum.auto(), """
+    Each shorter row of stars is between two longer rows, like the 50-star flag.
+    """
+
+    LONG_SANDWICH = enum.auto(), """
+    Each longer row of stars is between two shorter rows.
+    """
+
+    PAGODA = enum.auto(), """
+    Each longer row of stars is followed by a shorter row, like the 45-star flag.
+    """
+
+    @staticmethod
+    def from_layout(layout: tuple[int, int, int, int]) -> "LayoutKind":
+        a, b, c, d = layout
+        if not c:
+            return LayoutKind.GRID
+        if d == b:
+            return LayoutKind.QUINCUNX
+        if c == a - 1:
+            return LayoutKind.SHORT_SANDWICH
+        if c == a + 1:
+            return LayoutKind.LONG_SANDWICH
+        if c == a:
+            return LayoutKind.PAGODA
+        raise ValueError(f"Invalid layout: {layout}")
 
 def generate_star_layouts(nstars: int, _allow_extended_quincunx=False) -> Iterable[tuple[int, int, int, int]]:
     """
@@ -25,11 +68,7 @@ def generate_star_layouts(nstars: int, _allow_extended_quincunx=False) -> Iterab
     * elif c = a-1 (this condition may be optional), d is in (b-1, b)
     * else, d = b-1
 
-    When c is 0, the stars are arranged in a grid, like the 24-star "Old Glory" flag, or the 48-star flag.
-    When d is b, the stars are arranged in a quincunx, like the 49-star flag.
-    When c is a-1, each shorter row of stars is between two longer rows, like the 50-star flag.
-    When c is a+1, each longer row of stars is between two shorter rows.
-    When c is a, each longer row of stars is followed by a shorter row, like the 45-star flag.
+    In any such case, the number of rows is a + c and the number of columns is b + d.
     """
     # TODO: pass the extended quincunx option through the other functions
     for a in range(1, nstars + 1):
