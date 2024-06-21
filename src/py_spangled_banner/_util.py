7f3d24667460ee8445e33978a3fb0ef8f4ad858c
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-06-21 19:55:07 +0200

    Upgrade de write_from_get
    alignement sur la version de Parliamentarch
    TODO: en faire un module séparé

--- src/py_spangled_banner/_util.py
+++ src/py_spangled_banner/_util.py
@@ -9,12 +9,15 @@ _file_parameter = Parameter("file",
 def write_from_get(get_func: Callable[..., str]) -> Callable[..., None]:
     def write_func(file, /, *args, **kwargs):
         if isinstance(file, str):
-            with open(file, "w") as f:
+            with open(file, "w", encoding="UTF-8") as f:
                 return write_func(f, *args, **kwargs)
         print(get_func(*args, **kwargs), file=file)
 
     get_sig = signature(get_func)
-    write_sig = get_sig.replace(parameters=[_file_parameter]+list(get_sig.parameters.values()))
+    write_sig = get_sig.replace(
+        parameters=[_file_parameter]+list(get_sig.parameters.values()),
+        return_annotation=None,
+    )
     write_func.__signature__ = write_sig
     write_func.__name__ = get_func.__name__.replace("get_", "write_")
     return write_func
