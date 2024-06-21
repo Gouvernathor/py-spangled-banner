b25dba5bbe5e11c9092d27e4294ef6652d5215e3
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-06-21 20:06:15 +0200

    Add filter_kwargs to util
    imported from parliamentarch
    same, TODO make it a common utils module

--- src/py_spangled_banner/_util.py
+++ src/py_spangled_banner/_util.py
@@ -1,6 +1,7 @@
-from collections.abc import Callable
+from collections.abc import Callable, Container
 from inspect import signature, Parameter
 import io
+from typing import TypeVar
 
 _file_parameter = Parameter("file",
     Parameter.POSITIONAL_ONLY,
@@ -21,3 +22,25 @@ def write_from_get(get_func: Callable[..., str]) -> Callable[..., None]:
     write_func.__signature__ = write_sig
     write_func.__name__ = get_func.__name__.replace("get_", "write_")
     return write_func
+
+V = TypeVar("V") # PY3.11 compat
+def filter_kwargs(
+        *sets: Container[str],
+        **kwargs: V,
+        ) -> list[dict[str, V]]:
+    """
+    The length of the list is one more than the number of sets passed.
+    The sets may actually be any container.
+    """
+
+    rvdicts = []
+    for s in sets:
+        rvdict = {}
+        # no set operations in order to keep the ordering
+        for k in tuple(kwargs):
+            if k in s:
+                rvdict[k] = kwargs.pop(k)
+        rvdicts.append(rvdict)
+
+    rvdicts.append(kwargs)
+    return rvdicts
