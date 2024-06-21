c806680c1ae52feb7f382e170902501a5e59aabe
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-06-21 20:07:24 +0200

    Allow kwargs in get_svg
    keep an accurate signature as a bonus
    aligned over parliamentarch

--- src/py_spangled_banner/__init__.py
+++ src/py_spangled_banner/__init__.py
@@ -1,13 +1,34 @@
-from ._util import write_from_get
+from inspect import signature
+
+from ._util import filter_kwargs, write_from_get
 from .stars import find_best_star_layout
 from .geometry import Measurements
 from .svg import get_svg_from_layout
 
 __all__ = ("get_svg", "write_svg")
 
-def get_svg(nstars: int) -> str:
-    layout = find_best_star_layout(nstars)
-    measurements = Measurements.generate(star_layout=layout)
-    return get_svg_from_layout(measurements, layout)
+_FBSL_PARAM_NAMES = {k: p for k, p in signature(find_best_star_layout).parameters.items() if p.kind==p.KEYWORD_ONLY}
+_MG_PARAM_NAMES = {k: p for k, p in signature(Measurements.generate).parameters.items() if p.kind==p.KEYWORD_ONLY and k!="star_layout"}
+_GSFL_PARAM_NAMES = {k: p for k, p in signature(get_svg_from_layout).parameters.items() if p.kind==p.KEYWORD_ONLY}
+
+def get_svg(nstars: int, **kwargs) -> str:
+    fbsl_kwargs, mg_kwargs, gsfl_kwargs, kwargs = filter_kwargs(_FBSL_PARAM_NAMES, _MG_PARAM_NAMES, _GSFL_PARAM_NAMES, **kwargs)
+
+    if kwargs:
+        raise TypeError("Unknown parameters : " + ", ".join(kwargs))
+
+    layout = find_best_star_layout(nstars, **fbsl_kwargs)
+    measurements = Measurements.generate(star_layout=layout, **mg_kwargs)
+    return get_svg_from_layout(measurements, layout, **gsfl_kwargs)
+
+_sig = signature(get_svg)
+_nstars_param = _sig.parameters["nstars"]
+get_svg.__signature__ = _sig.replace(parameters=(
+    _nstars_param,
+    *_FBSL_PARAM_NAMES.values(),
+    *_MG_PARAM_NAMES.values(),
+    *_GSFL_PARAM_NAMES.values(),
+))
+del _sig, _nstars_param
 
 write_svg = write_from_get(get_svg)
